7ed34d295dcc412a613b00870f87c06d838c5ac7
hendisantika <hendisantika@yahoo.co.id>
2024-06-14 18:49:19 +0700

    Add BeforeAll in UserRepositoryTest🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/pastreamer/repository/UserRepositoryTest.java
+++ src/test/java/id/my/hendisantika/pastreamer/repository/UserRepositoryTest.java
@@ -1,9 +1,14 @@
 package id.my.hendisantika.pastreamer.repository;
 
 import com.speedment.jpastreamer.application.JPAStreamer;
+import id.my.hendisantika.pastreamer.entity.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +29,14 @@ class UserRepositoryTest {
     @Autowired
     private JPAStreamer jpaStreamer;
 
+    @BeforeAll
+    public void setup() {
+        Assertions.assertNotNull(jpaStreamer);
+
+        userRepository.save(new User(null, "username-1", LocalDate.of(1981, 01, 01), true));
+        userRepository.save(new User(null, "username-2", LocalDate.of(2001, 01, 01), true));
+        userRepository.save(new User(null, "username-3", LocalDate.of(2002, 01, 01), true));
+        userRepository.save(new User(null, "username-4", LocalDate.of(1984, 01, 01), false));
+        userRepository.save(new User(null, "username-5", LocalDate.of(1985, 01, 01), false));
+    }
 }
