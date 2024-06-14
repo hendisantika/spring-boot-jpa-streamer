004b5d45887b0103f07a9e9220435662d9e1ac43
hendisantika <hendisantika@yahoo.co.id>
2024-06-14 18:38:25 +0700

    Create Spring Boot Project🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/pastreamer/SpringBootJpaStreamerApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.pastreamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaStreamerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaStreamerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/pastreamer/SpringBootJpaStreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.pastreamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaStreamerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
